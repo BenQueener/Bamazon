5261ac59196eaa00a2c093f33621b6e39a7c92dd
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-02 20:17:16 -0500

    finished sql file

--- bamazonSQL.sql
+++ bamazonSQL.sql
@@ -1,15 +1,30 @@
 --create database
+CREATE DATABASE bamazonDB;
 
 --use database
+USE bamazonDB;
 
 -- create table called products
-
---put the following in the table
---item_id (unique id for each product)
---product_name (Name of product)
---department_name
---price (cost to customer)
---stock_quantity (how much of the product is available in stores)
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price INT default 0,
+    stock_quantity INT default 0,
+    PRIMARY KEY (id)
+);
 
 --Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+INSERT INTO products( product_name, department_name, price, stock_quantity)
+VALUES  ("ipod", "electronics", 600, 10),
+        ("laptop", "electronics", 2500, 10),
+        ("headphones", "electronics", 60, 20),
+        ("shirt", "clothing", 15, 30),
+        ("pants", "clothing", 60, 20),
+        ("socks", "clothing", 10, 40),
+        ("plates", "home goods", 30, 6),
+        ("mugs", "home goods", 10, 12),
+        ("glasses", "home goods", 10, 12),
+        ("saucers", "home goods", 15, 6);
+
 
