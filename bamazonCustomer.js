f05bf41908bcbdfc5f82bb3fe9c6b1ccef3b2b94
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-03 18:32:43 -0500

    Finished startShopping() and whatDoWeHave() function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,25 @@
 // put all my require packages
+var inquirer = require('inquirer');
+var mysql = require('mysql')
 
 //Connect to my server
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root'
+    password: "root",
+    database: 'bamazonDB'
+})
 
 //connect to the database
+connection.connect(function(err) {
+// in case it returns errors
+    if (err) throw err;
+    
+
+
+
+})
 
 //message 1: ask for id of product they want to buy
 //message 2: asl how many units of the product they would like to buy
