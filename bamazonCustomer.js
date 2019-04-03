5fd166c9cb574e359cdd28f56a4108a1741d2a2c
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-02 19:38:03 -0500

    pseudo code basic files.

---
+++ bamazonCustomer.js
@@ -0,0 +1,14 @@
+// put all my require packages
+
+//Connect to my server
+
+//connect to the database
+
+//message 1: ask for id of product they want to buy
+//message 2: asl how many units of the product they would like to buy
+
+//check if the store has enough for the order..."Insuffciant quantity"
+
+//if you have enough product for the order...
+//update the database for the remaining quantity
+//show the customer the total cost
---
+++ bamazonSQL.sql
@@ -0,0 +1,15 @@
+--create database
+
+--use database
+
+-- create table called products
+
+--put the following in the table
+--item_id (unique id for each product)
+--product_name (Name of product)
+--department_name
+--price (cost to customer)
+--stock_quantity (how much of the product is available in stores)
+
+--Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+
